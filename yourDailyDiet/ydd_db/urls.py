40a80b2675b477f550f8d7979256583dad90d1c1
Natalia Onishchuk <coffee.addicted.person@gmail.com>
2024-02-11 20:33:12 +0200

    Minor ui changes

--- yourDailyDiet/ydd_db/urls.py
+++ yourDailyDiet/ydd_db/urls.py
@@ -6,7 +6,8 @@ from .views.delete_meal import DeleteMealView
 from .views.meals_list import meals_list
 
 urlpatterns = [
-    path("", main_page, name="main"),
+    # path("", main_page, name="main"),
+    path('',  meals_list,  name='meals_list'),
     path('meals/add',  CreateNewMealView.as_view(),  name='add_meal'),
     path('meals/<uuid:pk>/delete',  DeleteMealView.as_view(),
          name='delete_meal'),
